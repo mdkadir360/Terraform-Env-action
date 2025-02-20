f5327403cef95388e77e9d48e4ce10d02d7d4393
Abdul Kadir <101203829+mdkadir360@users.noreply.github.com>
2025-02-20 12:29:32 +0530

    Update provider.tf

--- prod/provider.tf
+++ prod/provider.tf
@@ -9,6 +9,6 @@ terraform {
     resource_group_name   = "mat-delete-krna"
     storage_account_name  = "kadirst25"
     container_name        = "con"
-    key                   = "dev-terraform.tfstate"
+    key                   = "dev2-terraform.tfstate"
   }
 }
