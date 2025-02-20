265298692b8d93a3a6d8cb965784aa9a2b749794
Abdul Kadir <101203829+mdkadir360@users.noreply.github.com>
2025-02-20 12:29:19 +0530

    Update provider.tf

--- staging/provider.tf
+++ staging/provider.tf
@@ -5,10 +5,10 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "storage"
-    storage_account_name  = "2525storage"
-    container_name        = "stage"
-    key                   = "stage-terraform.tfstate"
+    resource_group_name   = "mat-delete-krna"
+    storage_account_name  = "kadirst25"
+    container_name        = "con"
+    key                   = "dev3-terraform.tfstate"
   }
   
 }
